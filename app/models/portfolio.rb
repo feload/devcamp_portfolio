cbf3c14bbdc19dbcf6064db30599e47854216b66
Felipe <feload@gmail.com>
2017-09-11 21:09:20 -0500

    Implemented concerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,13 @@
 class Portfolio < ApplicationRecord
+    include Placehoder
+
     validates_presence_of   :title, :body, :main_image, :thumb_image
 
     after_initialize :set_defaults
 
     def set_defaults
-        self.main_image ||= "http://placeholder.it/600x400"
-        self.thumb_image ||= "http://placeholder.it/350x200"
+        self.main_image ||= Placehoder.image_generator(height:600,width:400)
+        self.thumb_image ||=  Placehoder.image_generator(height:350,width:200)
     end
 
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,11 @@
 class Skill < ApplicationRecord
+    include Placehoder
+
     validates_presence_of   :title, :percent_utilized
+
+    after_initialized :set_defaults
+
+    def set_defaults
+        self.badge ||= Placehoder.image_generator(height: 250, width: 250)
+    end
 end
